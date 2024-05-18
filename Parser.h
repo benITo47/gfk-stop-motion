6280a8a42ffecd6f38208c49ae25120a8b6d93dc
frun36 <franek.urbanski@gmail.com>
2024-05-18 18:20:26 +0200

    Implement file reading

--- Parser.h
+++ Parser.h
@@ -6,11 +6,25 @@
 #include <string>
 #include <memory>
 #include <stdexcept>
+#include <iostream>
+#include <fstream>
 
 class Parser {
 private:
-
+	std::vector<std::string> _lines;
 public:
+	void readFile(std::string path) {
+		std::ifstream f;
+		f.open(path);
+		if (!f.is_open())
+			throw std::runtime_error("Couldn't open file: " + path);
+
+		std::string line;
+		while (std::getline(f, line))
+			_lines.push_back(line);
+	}
+
+
 	static std::string getName(std::string input) {
 		auto paren = input.begin() + input.find("(");
 		return std::string(input.begin(), paren);
